64c2f4c7f7a611b39ae0044e0b07ef1fa35e3e41
Wonjin Cho <cwonjin1@gmail.com>
2022-01-31 11:27:00 -0500

    Added: solution 2 added

--- 1.two-sum/solution1.js
+++ 1.two-sum/solution1.js
@@ -1,15 +1,15 @@
 const twoSum = (nums, target) => {
     
-    
+    const result = []
+
     for (let i=0; i<nums.length; i++)
-        
     for (let j=i+1; j<nums.length; j++){
-            if(nums[i]+nums[j] === target){
-                return [i,j]
+            if(nums[i]+nums[j] == target){
+                result.push(i,j)
             }
     }
     
-    
+    return result
 
 
 }
